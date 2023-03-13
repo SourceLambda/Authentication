319ece1f07ff565ae4dabae1ca5acdc88210ae4c
bchaparro11 <bchaparro@unal.edu.co>
2023-03-12 21:01:22 -0500

    Docker and First NodeJS is working

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("WORKING!")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+  res.send("Hello this is my first test with NodeJS");
+});
+
+app.listen(port,()=>{
+  console.log("This is working!");
+});
