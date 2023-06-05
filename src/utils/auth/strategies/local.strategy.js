3974c25f8983a0e9036dbfb8d449470ab92a2d6b
Brian Chaparro Cetina <bchaparro@unal.edu.co>
2023-06-04 21:49:51 -0500

    LDAP is part of the Two-Factor Authentication now

--- src/utils/auth/strategies/local.strategy.js
+++ src/utils/auth/strategies/local.strategy.js
@@ -4,7 +4,27 @@ const boom = require("@hapi/boom");
 
 const UserService = require("../../../services/user.service");
 
+const ldap = require("ldapjs");
+
 const service = new UserService();
+
+const authenticateDN = (username, password) => {
+  return new Promise((resolve, _ ) => {
+    const client = ldap.createClient({
+      url: ['ldap://172.30.0.1:389', 'ldap://172.30.0.1:636']
+    });
+
+    client.bind(username, password, (err) => {
+      if (err) {
+        console.log(`Error in new connection ${err}`);
+        resolve(false);
+      } else {
+        console.log("Authentication with LDAP is a success!");
+        resolve(true);
+      }
+    });
+  });
+};
 
 const LocalStrategy = new Strategy({
     usernameField:"email",
@@ -13,6 +33,7 @@ const LocalStrategy = new Strategy({
   async (email, password, done)=>{
     try {
       const user = await service.findByEmail(email);
+
       if (!user) {
         done(boom.unauthorized(),false);
       }
@@ -20,13 +41,26 @@ const LocalStrategy = new Strategy({
       const isMatch = await bcrypt.compare(password,user.password);
 
       if(!isMatch){
-        done(boom.unauthorized(),false);
+        done(boom.unauthorized("Password didn't match in the first part of Authentication"),false);
       }
+
+      //"arqsoft.unal.edu.co"
+      //cn=bchaparro@unal.edu.co,ou=sa,dc=arqsoft,dc=unal,dc=edu,dc=co
+      //authenticateDN("cn=admin,dc=arqsoft,dc=unal,dc=edu,dc=co","admin");
+      const ldapRespose = await authenticateDN(`cn=${email},ou=sa,dc=arqsoft,dc=unal,dc=edu,dc=co`,`${password}`);
+
+      if(!ldapRespose){
+        done(boom.unauthorized("LDAP authentication failed, but it worked in the first part of Authentication"),false);
+      }
+
       delete user.password;
       done(null, user);
     } catch (error) {
       done(error,false);
     }
 });
+
+
+
 
 module.exports = LocalStrategy;
